59b7ee251cc3aa9395f46ba400f49287694a894e
Marek Svorobovic <marek.svorobovic@gmail.com>
2021-05-30 10:15:33 +0300

    change default admin translations to english

--- app/code/community/UnzerDirect/Payment/Model/System/Config/Source/PaymentMethods.php
+++ app/code/community/UnzerDirect/Payment/Model/System/Config/Source/PaymentMethods.php
@@ -4,9 +4,9 @@ class UnzerDirect_Payment_Model_System_Config_Source_PaymentMethods
     public function toOptionArray()
     {
         return array(
-            array('value' => '', 'label' => Mage::helper('unzerdirect_payment')->__('Alle betalingsmetoder')),
-            array('value' => 'creditcard', 'label' => Mage::helper('unzerdirect_payment')->__('Alle kreditkort')),
-            array('value' => 'specified', 'label' => Mage::helper('unzerdirect_payment')->__('Som angivet')),
+            array('value' => '', 'label' => Mage::helper('unzerdirect_payment')->__('All payment methods')),
+            array('value' => 'creditcard', 'label' => Mage::helper('unzerdirect_payment')->__('All credit cards')),
+            array('value' => 'specified', 'label' => Mage::helper('unzerdirect_payment')->__('As defined')),
         );
     }
 }
