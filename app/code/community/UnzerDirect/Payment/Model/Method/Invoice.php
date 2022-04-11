a33eba5c438036bb29ca9f3d7658097bcfd1e2e1
Marek Svorobovic <marek.svorobovic@gmail.com>
2022-04-11 21:39:33 +0300

    payment title update, unzer invoice payment additional validation

--- app/code/community/UnzerDirect/Payment/Model/Method/Invoice.php
+++ app/code/community/UnzerDirect/Payment/Model/Method/Invoice.php
@@ -4,8 +4,30 @@ class UnzerDirect_Payment_Model_Method_Invoice extends UnzerDirect_Payment_Model
     protected $_code = 'unzerdirect_invoice';
     protected $_formBlockType = 'unzerdirect_payment/payment_form_invoice';
 
+    protected static $_cartAmountMin = 10;
+    protected static $_cartAmountMax = 3500;
+
+    protected $_allowCurrencyCode = array(
+        'EUR', 'CHF'
+    );
+
     public function getPaymentMethods()
     {
         return 'unzer-pay-later-invoice';
+    }
+
+    public function isAvailable($quote = null)
+    {
+        if($quote) {
+            if (!in_array($quote->getBaseCurrencyCode(), $this->_allowCurrencyCode)) {
+                return false;
+            }
+
+            if($quote->getGrandTotal() < self::$_cartAmountMin
+                || $quote->getGrandTotal() > self::$_cartAmountMax) {
+                return false;
+            }
+        }
+        return parent::isAvailable($quote);
     }
 }
